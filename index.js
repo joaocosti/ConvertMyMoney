42d0c48ad26127adb11f3e05c42222de8cb2d0d3
joaocosti <joao_costi@hotmail.com>
2019-09-10 18:13:56 -0300

    Versão 1.0

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+//Para funcionar no Zeit
+const path = require('path')
+
+const convert = require('./lib/convert')
+//Para o ejs
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
+//Pasta para colocar o CSS
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res) => {
+    res.render('home')
+})
+
+app.get('/cotacao', (req,res) => {
+    const {cotacao, quantidade} = req.query
+    if(cotacao && quantidade){
+    const conversao = convert.convert(cotacao,quantidade)
+    res.render('cotacao',{
+        error: false,
+        cotacao: convert.toMoney(cotacao),
+        quantidade: convert.toMoney(quantidade),
+        conversao: convert.toMoney(conversao)
+    })
+    }
+    else {
+        res.render('cotacao',{
+            error: 'Valores Inválidos'
+        })
+    }
+})
+
+app.listen(3000, err =>{
+    if(err){
+        console.log('Não foi possível iniciar o servidor')
+    }
+    else{
+        console.log('ConvertMyMone está online')
+    }
+})
